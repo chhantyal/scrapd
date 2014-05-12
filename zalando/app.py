2c6a6d555cc0226116210ec49d0feea826513902
Nar Kumar <neokya@gmail.com>
2014-05-12 21:23:18 +0530

    add command line option

--- zalando/app.py
+++ zalando/app.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
+import argparse
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -8,5 +11,12 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+def cli_options(args):
+    description = ('Run flask app to scrap zalando.de')
+    parser = argparse.ArgumentParser(description=description)
+    parser.add_argument('--port', type=int)
+    return parser.parse_args()
+
 if __name__ == '__main__':
-    app.run()
+    args = cli_options(sys.argv)
+    app.run(port=args.port)
